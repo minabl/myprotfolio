3e19c3ea7fd7e0a874b6bb435eb5e28f9742bb7e
mina <blilidhmina@gmail.com>
2024-11-10 13:41:05 +0100

    update config2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-export const reactStrictMode = true;
-export const env = {
-	dir: '/',
-};
+   
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+	/* config options here */
+	reactStrictMode: true,
+	env: {
+	  dir: '/',
+	},
+	images: {
+	  remotePatterns: [
+		{
+		  protocol: 'https',
+		  hostname: '**.github.io',
+		  pathname: '/badge/**',
+		},
+	  ],
+	},
+  }
+   
+  export default nextConfig
