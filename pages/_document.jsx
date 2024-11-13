8f39ba92c4a7d1436f362e21b374428026259bc3
mina <blilidhmina@gmail.com>
2024-11-13 06:56:38 +0100

    featured update1

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -14,7 +14,7 @@ export default function Document() {
 				<link rel="apple-touch-icon" sizes="180x180" href="myprotfolio/favicon/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="myprotfolio/favicon/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="myprotfolio/favicon/favicon-16x16.png" />
-				<link rel="manifest" href="myprotfolio/favicon/site.webmanifest" />
+				<link rel="manifest" href="/favicon/site.webmanifest" />
 				<link rel="mask-icon" href="myprotfolio/favicon/safari-pinned-tab.svg" color="#5bbad5" />
 				<meta name="msapplication-TileColor" content="#da532c" />
 				<meta name="theme-color" content="#ffffff" />
